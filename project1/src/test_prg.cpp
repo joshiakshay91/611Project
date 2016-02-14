fbe56c7ef0941757a3741927c936ea8e428cf983
Akshay Joshi <joshiaks91@gmail.com>
2016-02-13 23:48:43 -0800

    player moments

--- project1/src/test_prg.cpp
+++ project1/src/test_prg.cpp
@@ -11,12 +11,18 @@
 #include <semaphore.h>
 #include <errno.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+
 struct GameBoard{
 	int rows;
 	int coloumns;
 	unsigned char players;
-	unsigned char map[0];
-}
+	unsigned char mapya[0];
+};
 
 using namespace std;
 int main()
@@ -45,12 +51,14 @@ int main()
 		sem_getvalue(mysemaphore,&mysemaphoreVal);
 		sem_wait(mysemaphore);
 
-		int fd= shm_open("/AJGoldmem", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-		if(fd=-1)
+		int fd = shm_open("/APJMEMORY", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+		if(fd==-1)
 		{
 			perror("Shared memory creation failed");
 			exit(1);
 		}
+
+
 
 		ifstream in("mymap.txt");
 		getline(in,line);
@@ -64,12 +72,24 @@ int main()
 		const char* data = text.c_str();
 		int area = line_length*num_lines;
 		char map[area];
+
+		if((ftruncate(fd,(area + sizeof(GameBoard))))==-1)
+		{
+			perror("FTRUNCATE FAILURE :");
+			exit(1);
+		}
+		GameBoard* Goldberg= (GameBoard*) mmap(NULL, area, PROT_READ|PROT_WRITE, MAP_SHARED,fd, 0);
+  //  Goldberg->mapya=text.c_str();
+
+
+
 		std::uniform_int_distribution<int> rand(1,area); //here
 		engi.seed(aj());
-
+		counter++;
 		//char map[]; //something arbitrarily large for this test
 		const char* ptr=data;
 		char* mp=map;
+		char* PL1=map;
 		//Convert the ASCII bytes into bit fields drawn from goldchase.h
 		while(*ptr!='\0')
 		{
@@ -90,8 +110,13 @@ int main()
 			}
 			if(*ptr==' ')
 			{
-				if(counter==1){
-					*mp=G_GOLD;
+				if(counter<=2){
+					if(counter==1){
+					*mp=G_GOLD;}
+					else{
+							*mp=G_PLR0;
+							PL1=mp;
+					}
 				}else{
 					*mp=G_FOOL;}
 				counter--;
@@ -100,10 +125,69 @@ int main()
 		Map goldMine(map,num_lines,line_length);
 		int a=0;
 		goldMine.postNotice("This is a notice");
-		while(a=goldMine.getKey()!='Q'){}
+		while(a=goldMine.getKey()!='Q'){
+     if(goldMine.getKey()=='H'||goldMine.getKey()=='h')
+		 {
+			 mp=map;
+			 mp=PL1;
+			 mp--;
+			 if(*mp!=G_WALL){
+				 mp++;
+				 *mp=0;
+				 mp--;
+				 PL1=mp;
+				 *mp=G_PLR0;
+			 }
+			 goldMine.drawMap();
+		 }
+		 else if(goldMine.getKey()=='K'|| goldMine.getKey()=='k')
+		 {
+			 mp=map;
+			 mp=PL1;
+			 mp++;
+			 if(*mp!=G_WALL){
+				 mp--;
+				 *mp=0;
+				 mp++;
+				 PL1=mp;
+				 *mp=G_PLR0;
+			 }
+			 goldMine.drawMap();
+		 }
+		 else if(goldMine.getKey()=='J'|| goldMine.getKey()=='j')
+		 {
+			 mp=map;
+			 mp=PL1;
+			 mp-=line_length;
+			 if(*mp!=G_WALL){
+				 mp+=line_length;
+				 *mp=0;
+				 mp-=line_length;
+				 PL1=mp;
+				 *mp=G_PLR0;
+			 }
+			 goldMine.drawMap();
+		 }
+		 else if(goldMine.getKey()=='L'|| goldMine.getKey()=='l')
+		 {
+			 mp=map;
+			 mp=PL1;
+			 mp+=line_length;
+			 if(*mp!=G_WALL){
+				 mp=mp-line_length;
+				 *mp=0;
+				 mp+=line_length;
+				 PL1=mp;
+				 *mp=G_PLR0;
+			 }
+			 goldMine.drawMap();
+		 }
+
+		}
 		sem_post(mysemaphore);
 		sem_close(mysemaphore);
-		sem_unlink("TAGgoldchase");
+		shm_unlink("/APJMEMORY");
+		sem_unlink("APJgoldchase");
 	}//here semaphore not failed if ends
 
 }
